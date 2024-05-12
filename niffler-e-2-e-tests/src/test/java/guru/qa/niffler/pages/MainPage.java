335c32652168439e44de6d8a24baae344f0680c1
Silver <testingbugsrus@gmail.com>
2024-05-12 13:56:41 +0300

    Add PO MainPage.java

---
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/pages/MainPage.java
@@ -0,0 +1,33 @@
+package guru.qa.niffler.pages;
+
+
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+import guru.qa.niffler.model.SpendJson;
+
+import static com.codeborne.selenide.CollectionCondition.size;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+
+public class MainPage {
+    private final ElementsCollection spendingRows = $(".spendings-table tbody").$$("tr");
+    private final SelenideElement deleteSpendingButton = $(".spendings__bulk-actions button");
+
+
+    public SelenideElement findSpendingByDescription(String description) {
+        return spendingRows.find(text(description));
+    }
+
+    public void chooseSpending(SpendJson spendJson) {
+        findSpendingByDescription(spendJson.description())
+                .$$("td").first().scrollIntoView(true).click();
+    }
+
+    public void deleteSpending() {
+        deleteSpendingButton.click();
+    }
+
+    public void checkSpendingWasDeleted(int size) {
+        spendingRows.shouldHave(size(size));
+    }
+}
